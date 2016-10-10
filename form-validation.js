bf30cd4cf0cabb56e3a9d1eb58652a112d83673f
DESKTOP-125OUER\Mitch <mskinner6@mail.csuchico.edu>
2016-10-10 16:48:27 -0700

    Added if statement for usernames if error and succcess Added if statement for password if password is "password" and if not its success.

--- form-validation.js
+++ form-validation.js
@@ -3,17 +3,28 @@ function validate(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
 
-  //Show message that there is an error with the username...
+  var txt = userEntered;
+  if(txt.length < 6  )
+  {
   document.getElementById("usernameError").innerHTML="Bad username.";
   document.getElementById("usernameError").classList.remove("hidden-message");
   document.getElementById("usernameError").classList.add("shown-message");
   //Turn the username items red
   document.getElementById("usernameGroup").classList.add("has-error");
-
+  }
+  else {
+    document.getElementById("usernameGroup").classList.add("has-success");
+  }
   //Show message that there is an error with the password...
+  if(passEntered == "password")
+  {
   document.getElementById("passwordError").innerHTML="Bad password.";
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
   document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else{
+    document.getElementById("passwordGroup").classList.add("has-success");
+  }
 }
